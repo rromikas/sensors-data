faeddae64a82e00c4bdcc90fedc8ffd783e1c828
rromikas <rromikas@gmail.com>
2021-01-25 22:28:41 +0200

    ok

--- src/api/index.js
+++ src/api/index.js
@@ -12,48 +12,6 @@ export const GetSecureCookie = (emailId) => {
     });
 };
 
-const queuingStrategy = new CountQueuingStrategy({ highWaterMark: 1 });
-const writableStream = new WritableStream(
-  {
-    write(chunk) {
-      console.log("chunk", chunk);
-      return new Promise((resolve, reject) => {
-        resolve();
-      });
-    },
-    abort(err) {
-      console.log("Sink error:", err);
-    },
-  },
-  queuingStrategy
-);
-const defaultWriter = writableStream.getWriter();
-
-function sendMessage(message, writableStream) {
-  defaultWriter.ready
-    .then(() => {
-      return defaultWriter.write(message);
-    })
-    .then(() => {
-      console.log("Chunk written to sink.");
-    })
-    .catch((err) => {
-      console.log("Chunk error:", err);
-    });
-  // Call ready again to ensure that all chunks are written
-  //   before closing the writer.
-  defaultWriter.ready
-    .then(() => {
-      defaultWriter.close();
-    })
-    .then(() => {
-      console.log("All chunks written");
-    })
-    .catch((err) => {
-      console.log("Stream error:", err);
-    });
-}
-
 export const SendSensorData = (sensorType, sensorData) => {
   return fetch(`${apiUrlOrigin}/sendSensorData`, {
     method: "POST",
